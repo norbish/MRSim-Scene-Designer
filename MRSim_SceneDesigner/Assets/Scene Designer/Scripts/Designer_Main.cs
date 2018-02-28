25f8c3ced339f37c790e53d50dada51fc7a6f5d9
norbish <torsun@hotmail.com>
2018-02-28 19:39:31 +0100

    layout done

---
+++ MRSim_SceneDesigner/Assets/Scene Designer/Scripts/Designer_Main.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Designer_Main : MonoBehaviour {
+
+    public GameObject scrollArea;
+    public GameObject Module_Alignment;
+    RectTransform panel_rect;
+    // Use this for initialization
+    
+	void Start ()
+    {
+        panel_rect = scrollArea.GetComponent<RectTransform>();
+
+        int count = 0;
+        Vector2 moduleBox_size = new Vector2(8.5f, 4);
+        float height = 100;
+        float offset = 0;
+
+        List<Parameters> modules = new List<Parameters>();
+        for(int i = 0; i<3; i++)
+        {
+            modules.Add(new Parameters()
+            {
+                name = "Module" + i,
+                color = new Color(0, 0, i * 0.3f),//do gray/lightGray every 2nd module
+                position = new Vector2(panel_rect.rect.center.x, panel_rect.rect.yMax - i * moduleBox_size.y * height),// panel_Content.GetComponent<RectTransform>().rect.center + new Vector2(0,235), //panel_Content.transform.position,//new Vector2(Module_Alignment.transform.position.x,0),
+                size = moduleBox_size,
+                parent = scrollArea,
+            });
+            
+            
+            modules[i].Create();
+            offset += height * moduleBox_size.y;
+            
+        }
+        //panel_rect.up = new Vector2(0,-offset);
+        panel_rect.offsetMin = new Vector2(0,offset);
+        //panel_rect.offsetMax = new Vector2(0, -offset);
+
+
+        /*float width = mod1.panel.GetComponent<RectTransform>().rect.width;
+        float ht = mod1.panel.GetComponent<RectTransform>().rect.height;*/
+
+        count++;
+
+        //panel_Content.GetComponent<RectTransform>().offsetMin = new Vector2(width, -ht * count);
+        //panel_Content.GetComponent<RectTransform>().offsetMax = new Vector2 (width,ht*count);
+
+        //panel.GetComponent<RectTransform>().transform.position = new Vector2(-600,0);
+
+        //panelTransform.offsetMax = new Vector2(700,800);
+        //panel.transform.SetParent(canvas.transform);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ MRSim_SceneDesigner/Assets/Scene Designer/Scripts/Parameters.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Parameters {
+
+    public GameObject panel;
+    public string name;
+    public Color color;
+    public Vector2 position;
+    public Vector2 size;
+    public GameObject parent;
+    private RectTransform rect;
+    public Parameters()
+    {
+
+    }
+
+    public void Create()
+    {
+        panel = new GameObject(name);
+        rect = panel.AddComponent<RectTransform>();
+        
+        panel.transform.SetParent(parent.transform);
+        panel.GetComponent<RectTransform>().transform.localScale = size;
+        panel.transform.localPosition = position - new Vector2(0, 0);//(rect.rect.height /** size.y)/2*/));
+        //panel.transform.position = position;// + new Vector2(135,-130);
+
+        Image i = panel.AddComponent<Image>();
+        i.color = color;
+        //add textboxes, sliders, etc. 
+    }
+
+    public void AddVectorInput()
+    {
+        //add vector textboxes
+    }
+}
